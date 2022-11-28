a2acf55fe79a2ff5ac44c60d0ef77b6eb63681b0
Ali Naderi <104677416+Alinaderi1378@users.noreply.github.com>
2022-11-28 13:42:59 +0330

    firstcommit

---
+++ main.go
@@ -0,0 +1,177 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	run := true
+
+	fmt.Printf("این یک پروژه ی ماشین حساب است.\nتوابعی که میتوان با این ماشین حساب انجام داد : عملیات جمع وتفریق و ضرب و تقسیم و سینوس و کو\nلطفابرای خروج کلید q  را بزنید.\n--------------------------------------\n")
+
+	// reads user input
+	reader := bufio.NewReader(os.Stdin)
+	for run {
+		fmt.Printf("لطفا یک عدد را وارد کنید: ")
+		first, _ := reader.ReadString('\n')
+
+		var trimmed = trim(first, "\r\n")
+		if shouldQuit(trimmed) {
+			fmt.Printf("خروج")
+			os.Exit(0)
+		}
+		num1, _ := strconv.ParseFloat(trimmed, 64)
+
+		fmt.Printf("Enter your operand: ")
+		op, _ := reader.ReadString('\n')
+
+		var optrim = trim(op, "\r\n")
+		if shouldQuit(optrim) {
+			fmt.Printf("خروج")
+			os.Exit(0)
+		}
+		var vo = validOp(optrim)
+		for !vo {
+			fmt.Printf("عملگر ناشناخته، لطفا یک عملگرجدید را وارد کنید ")
+			op, _ := reader.ReadString('\n')
+			optrim = trim(op, "\r\n")
+			if shouldQuit(optrim) {
+				fmt.Printf("Quitting...")
+				os.Exit(0)
+			}
+			vo = validOp(optrim)
+		}
+
+		if optrim == "sin" || optrim == "cos" || optrim == "tan" || optrim == "log" || optrim == "sqrt" {
+			fmt.Printf("جواب: " + callCalculation_once(optrim, num1) + "\n--------------------------------------\n")
+
+		} else {
+
+			fmt.Printf("عدد دوم را وارد کنید: ")
+			second, _ := reader.ReadString('\n')
+
+			// حذف خطوط جدید با تریم
+			trimmed = trim(second, "\r\n")
+			if shouldQuit(trimmed) {
+				fmt.Printf("خروج")
+				os.Exit(0)
+			}
+			num2, _ := strconv.ParseFloat(trimmed, 64)
+
+			fmt.Printf("جواب: " + callCalculation_twice(optrim, num1, num2) + "\n--------------------------------------\n")
+		}
+
+	}
+}
+
+// فراخوانی توابع ماشین حساب
+func callCalculation_twice(op string, a, b float64) string {
+	if op == "+" {
+		return convertFloat64ToString(addition(a, b))
+	} else if op == "-" {
+		return convertFloat64ToString(subtract(a, b))
+	} else if op == "*" || op == "x" || op == "X" {
+		return convertFloat64ToString(multiply(a, b))
+	} else if op == "/" {
+		return convertFloat64ToString(divide(a, b))
+	} else if op == "pow" {
+		return convertFloat64ToString(float64(pow(a, b)))
+	}
+	return "Couldn't calculate correctly."
+}
+func callCalculation_once(op string, a float64) string {
+	if op == "sin" {
+		return convertFloat64ToString(float64(sin(a)))
+	} else if op == "cos" {
+		return convertFloat64ToString(float64(cos(a)))
+	} else if op == "tan" {
+		return convertFloat64ToString(float64(tan(a)))
+	} else if op == "log" {
+		return convertFloat64ToString(float64(logarithm(a)))
+	} else if op == "sqrt" {
+		return convertFloat64ToString(float64(sqrt(a)))
+	}
+	return "Couldn't calculate correctly."
+}
+
+// عملگر های شناخته شده
+func validOp(op string) bool {
+
+	validOps := []string{"+", "-", "*", "x", "X", "/", "sin", "cos", "tan", "log", "sqrt", "pow"}
+	for _, validOp := range validOps {
+		if validOp == op {
+			return true
+		}
+	}
+	return false
+}
+
+// تابع خروج
+func shouldQuit(trimmed string) bool {
+	if trimmed == "q" {
+		return true
+	}
+	return false
+}
+
+func convertFloat64ToString(float float64) string {
+	return strconv.FormatFloat(float, 'f', -1, 64)
+}
+func trim(value, cutset string) string {
+	return strings.Trim(value, cutset)
+}
+
+// جمع
+func addition(first, second float64) float64 {
+	return first + second
+}
+
+// منها
+func subtract(first, second float64) float64 {
+	return first - second
+}
+
+// ضرب
+func multiply(first, second float64) float64 {
+	return first * second
+}
+
+// تقسیم
+func divide(first, second float64) float64 {
+	return first / second
+}
+
+// سینوس
+func sin(first float64) float64 {
+	return math.Sin(first)
+}
+
+// کوسینوس
+func cos(first float64) float64 {
+	return math.Cos(first)
+}
+
+// تانژانت
+func tan(first float64) float64 {
+	return math.Tan(first)
+}
+
+// لگاریتم
+func logarithm(first float64) float64 {
+	return math.Log10(first)
+}
+
+// جذر
+func sqrt(first float64) float64 {
+	return math.Sqrt(first)
+}
+
+// توان
+func pow(first, second float64) float64 {
+	return math.Pow(first, second)
+}
